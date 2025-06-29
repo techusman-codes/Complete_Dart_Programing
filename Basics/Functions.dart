37d8f598ccc8365d989aa741295cba3e738ece4e
Usman Umar Garba <ugarba202@gmail.com>
2025-06-29 15:52:45 +0100

    functions

---
+++ Basics/Functions.dart
@@ -0,0 +1,32 @@
+void main() {
+  // ****************** Functions *****************//
+  // Functions this are block of codes that perform a specific task
+
+  // returnType  functionNmae(parameter){
+  // code eecution}
+
+  // write a function that print in the console.
+
+  // parameter is what we pass to the function
+  // method is a function that is inside a class
+
+  // const String name = "Usman Umar Garba";
+  // printSomething(name);
+
+  // passing the value of the function
+  const a = 10;
+  const b = 20;
+  addNumber(a, b);
+}
+
+// void printSomething(String param) {
+//   print(param);
+// }
+
+// write a function that add two numbers and return the result
+void addNumber(int a, int b) {
+  print(a + b);
+}
+
+// returnType functions this are function that return a value after exexcution
+
