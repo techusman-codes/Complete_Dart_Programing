842aed1055c5ee255a3394f4aeccbf4917cb6067
Usman Umar Garba <ugarba202@gmail.com>
2025-07-01 23:47:31 +0100

    conditions

---
+++ Basics/conditions.dart
@@ -0,0 +1,40 @@
+// *******************Conditions in Dart*******************//
+// Conditions this is what you will be telling the computer to do based on conditions//
+
+// void main() {
+//   // if, else if, else, switch
+
+//   const age = 18;
+//   checkAge(10);
+// }
+
+// void checkAge(int age) {
+//   if (age > 18) {
+//     print("you are an adult");
+//   } else {
+//     print('you are not an adult');
+//   }
+// }
+
+// if else if //
+
+void main() {
+  const age = 15;
+  checkAge(age);
+}
+
+// void checkAge(int age) {
+//   if (age > 18) {
+//     print('you are an adult');
+//   } else if (age == 15) {
+//     print("you are 15 years old");
+//   } else if (age == 18) {
+//     print('you are 18 years old');
+//   } else {
+//     print("you are not an adult");
+//   }
+// }
+
+
+// Switch Statement//
+// write a function that get the month and retur
